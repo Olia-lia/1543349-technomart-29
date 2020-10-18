7eb7ada2a78a19fc70fe07f6dadf186e760397ae
Olga Lee <olyalee100@gmail.com>
2020-10-19 01:22:48 +0300

    Завершена верстка и интерактивные элементы

---
+++ js/script.js
@@ -0,0 +1,130 @@
+
+//Форма обратной связи
+
+const modalFeedback = document.querySelector(".modal-feedback");
+const openForm = document.querySelector(".open-form");
+const closeForm = modalFeedback.querySelector(".modal-close");
+const modalForm = modalFeedback.querySelector(".feedback-form");
+const nameFieldForm = modalFeedback.querySelector(".form-name");
+const emailFieldForm = modalFeedback.querySelector(".form-email");
+const textFieldForm = modalFeedback.querySelector(".form-text");
+
+const isStorageSupport = true;
+const storage = "";
+
+openForm.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	modalFeedback.classList.add("modal-show");
+  nameFieldForm.focus();
+});
+
+closeForm.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	modalFeedback.classList.remove("modal-show");
+ });
+
+
+
+modalForm.addEventListener("submit", function (evt) {
+   
+    if (!nameFieldForm.value || !emailFieldForm.value || !textFieldForm.value) {
+    	 evt.preventDefault();}
+    	 else  {
+    	 if (isStorageSupport) {
+      localStorage.setItem("emaill", emailFieldForm.value);
+  		}
+    }
+});
+
+
+document.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (modalFeedback.classList.contains("modal-show")) {
+      evt.preventDefault();
+      modalFeedback.classList.remove("modal-show");
+    }
+  }
+});
+
+
+
+//Карта
+
+const mapLink = document.querySelector(".modal-map-link");
+const mapPopup = document.querySelector(".modal-map");
+const mapClose = mapPopup.querySelector(".modal-close");
+
+mapLink.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  mapPopup.classList.add("modal-show-map");
+});
+
+mapClose.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  mapPopup.classList.remove("modal-show-map");
+});
+
+
+document.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (mapPopup.classList.contains("modal-show-map")) {
+      evt.preventDefault();
+      mapPopup.classList.remove("modal-show-map");
+    }
+  }
+});
+
+
+//Слайдер
+
+const slides = document.querySelectorAll(".promo-slide"); 
+const controls = document.querySelectorAll(".slider-control");
+const nextSlide = document.querySelector(".slider-arrow-right");
+const previousSlide = document.querySelector(".slider-arrow-left");
+
+
+let slideNumber = 1;
+
+showSlide(slideNumber);
+
+function showSlide(n) {
+  let i;
+  if (n > slides.length) {
+    slideNumber = 1;
+  }
+  if (n < 1) {
+    slideNumber = slides.length;
+  }
+    for (i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("slide-current");
+  }
+      for (i = 0; i < controls.length; i++) {
+    controls[i].classList.remove("slider-control-current"); 
+  }
+
+    slides[slideNumber-1].classList.add("slide-current");
+    controls[slideNumber-1].classList.add("slider-control-current");};
+
+
+function nextSlides(n) {
+  showSlide(slideNumber += 1);
+}
+function currentSlide (n) {
+  showSlide(slideNumber = n);
+}
+nextSlide.onclick = function() {
+  nextSlides(-1);
+}
+
+previousSlide.onclick = function() {
+  nextSlides(+1);
+}  
+
+
+
+
+
+
+
+
+
